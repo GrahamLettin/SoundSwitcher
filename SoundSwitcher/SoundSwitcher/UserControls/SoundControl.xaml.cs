9156e23b892d1d05b6269c7de1ee6b2a3baaba02
Graham Lettin <grahamlettin@hotmail.com>
2022-08-27 10:25:35 +0100

    Added basic control to use for sound control

---
+++ SoundSwitcher/SoundSwitcher/UserControls/SoundControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SoundSwitcher.UserControls
+{
+    /// <summary>
+    /// Interaction logic for SoundControl.xaml
+    /// </summary>
+    public partial class SoundControl : UserControl
+    {
+        public SoundControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
