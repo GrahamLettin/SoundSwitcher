40ba2fd97d3a61a3d8456bb933194e022c72b966
Graham Lettin <grahamlettin@hotmail.com>
2022-08-27 11:07:50 +0100

    Created simple model for audio device - likely to change

---
+++ SoundSwitcher/SoundSwitcher/Models/AudioDevice.cs
@@ -0,0 +1,8 @@
+﻿namespace SoundSwitcher.Models
+{
+    internal class AudioDevice
+    {
+        public int ID { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
+}
